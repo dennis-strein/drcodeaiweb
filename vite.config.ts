e7759a8b94a608c84828784fcfacdf2b0e88a6c5
Dr. Code <85127662+dennis-strein@users.noreply.github.com>
2026-03-31 14:27:20 +0200

    Convert to static HTML site

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './public/index.html',
+        impressum: './public/impressum.html',
+        privacy: './public/privacy.html',
+        deleteAccount: './public/delete-account.html',
+        stylechatai: './public/stylechatai.html',
+        404: './public/404.html',
+      },
+    },
   },
+  publicDir: 'public',
 });
